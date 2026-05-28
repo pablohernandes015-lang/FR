b7960f8fb8a3d3b2f19baea3ac33efe93f799bae
RaulCasquinha <A50394@alunos.isel.pt>
2026-05-28 17:10:25 +0100

    Adicionar ficheiro colisao

---
+++ impl2/src/Colisao.java
@@ -0,0 +1,13 @@
+public class Colisao {
+
+    public static double anguloParede(double dist0, double dist1, double drobot1) {
+        return Math.atan(Math.abs(dist0 - dist1)/drobot1);
+    }
+
+    public static double distanciaPerpendicularParede(double dist0, double dist1, double drobot1) {
+        double a = anguloParede(dist0, dist1, drobot1);
+        return dist1 * Math.cos(a);
+    }
+
+    public static double 
+}
